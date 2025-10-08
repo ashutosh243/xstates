1e03dfe73b6cabc59fab26d4ac85502e521c49d8
ashutoshgupt243 <vnsashutoshit.gmail.com>
2025-10-08 15:34:31 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import Style from './app.module.css';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+  const [country, setCountry] = useState([]);
+  const [state, setState] = useState([]);
+  const [city, setCity] = useState([]);
+  const [selected, setSelected] = useState({ country: "", state: "", city: "" });
+
+  const changeHandler = (e) => {
+
+    setSelected((prev) => {
+
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+  useEffect(() => {
+
+    async function getCountry() {
+      try {
+        const response = await axios('https://crio-location-selector.onrender.com/countries');
+        setCountry(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    getCountry();
+
+  }, []);
+  useEffect(() => {
+
+    async function getState() {
+      try {
+        const response = await axios(`https://crio-location-selector.onrender.com/country=${selected.country}/states`);
+        setState(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    getState();
+
+  }, [selected.country]);
+  useEffect(() => {
+
+    async function getState() {
+      try {
+        const response = await axios(`https://crio-location-selector.onrender.com/country=${selected.country}/state=${selected.state}/cities`);
+        setCity(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    getState();
+
+  }, [selected.state]);
+  // console.log("selected data",selected);
+  // console.log("country data",country);
+  // console.log("state date",state);
+  // console.log("city data",city);
+  return (<>
+    <div className={Style.heading}>
+    <h1 >Select Location</h1>
+    </div>
+    <div className={Style.container}>
+
+      <select name='country' onChange={changeHandler}>
+      {
+          country?.map((name) => { return <option key={name} value={name}>{name}</option> })
+      }
+      </select>
+
+      <select name='state' onChange={changeHandler} disabled={selected.country===''?true:false}>
+        {
+          state?.map((name) => { return <option key={name} value={name}>{name}</option> })
+        }
+      </select>
+
+      <select name='city' onChange={changeHandler} disabled={selected.state===''?true:false}>
+        {
+          city?.map((name) => { return <option key={name} value={name}>{name}</option> })
+        }
+      </select>
+      
+    </div>
+    <div className={Style.selected}>
+    {selected.city!==''&&<p>{`You selected ${selected.city}, ${selected.state}, ${selected.country}`}</p>}
+    </div>
+  </>)
 }
 
-export default App
+export default App;
